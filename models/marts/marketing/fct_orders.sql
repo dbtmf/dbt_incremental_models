7627b10d1394a7380a3ecefacc67d2a13596602a
Adrian Torres Roda <materializationsfundamentals@gmail.com>
2025-09-23 12:01:13 +0000

    INTERMEDIATE_AND_MARTS_2

---
+++ models/marts/marketing/fct_orders.sql
@@ -0,0 +1,34 @@
+with 
+
+order as (
+
+    select 
+        order_id,
+        count(customer_id) as num_customers
+    from 
+        {{ref('stg_orders')}}
+    group by 1
+),
+
+payments as (
+    
+    select
+        order_id,
+        sum(payments_amount) as total_amount
+    from 
+        {{ref('stg_payments')}}
+    group by 1
+),
+
+final as (
+
+    select
+        a.order_id,
+        a.num_customers,
+        b.total_amount
+    from 
+        orders as a left join payments as b using(order_id)
+    order by 1
+)
+
+select * from final
