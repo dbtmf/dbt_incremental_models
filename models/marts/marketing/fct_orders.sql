7040825629c1751e74fc6af1cb4eab582b0301a9
Adrian Torres Roda <materializationsfundamentals@gmail.com>
2025-09-26 08:31:49 +0000

    MERGE_STRATEGY

--- models/marts/marketing/fct_orders.sql
+++ models/marts/marketing/fct_orders.sql
@@ -1,6 +1,8 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+        unique_key = 'order_id',
+        incremental_strategy = 'merge'
     )
 }}
 
@@ -56,4 +58,3 @@ incrementals AS (
 
 
 select * from incrementals
-
